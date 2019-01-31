b8b583628297269bebe4916eed63ef4e1fa1cc6c
Nahuel Zapata <inahuelzapata@gmail.com>
2019-01-31 11:31:07 -0300

    Add initial project settings.

---
+++ SELogging/SELogging/SELogging.h
@@ -0,0 +1,19 @@
+//
+//  SELogging.h
+//  SELogging
+//
+//  Created by Nahuel Zapata on 1/31/19.
+//  Copyright © 2019 iNahuelZapata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SELogging.
+FOUNDATION_EXPORT double SELoggingVersionNumber;
+
+//! Project version string for SELogging.
+FOUNDATION_EXPORT const unsigned char SELoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SELogging/PublicHeader.h>
+
+
